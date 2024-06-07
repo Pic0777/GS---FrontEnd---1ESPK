dba96341933d4242bc29a5800f049838eb5d3d58
LucianoMeriato <113532722+LucianoMeriato@users.noreply.github.com>
2024-06-07 17:26:48 -0300

    pag login e cadastro

--- js/login.js
+++ js/login.js
@@ -1,13 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Simular uma validação de login
-    if (username === 'user' && password === 'password') {
-        alert('Login bem-sucedido!');
-        window.location.href = '/index.html'; // Redirecionar para a página inicial
-    } else {
-        alert('Usuário ou senha inválidos.');
-    }
-});
+function login() {
+    alert('Login bem-sucedido!');
+    return true; // Permite que o formulário seja enviado
+}
